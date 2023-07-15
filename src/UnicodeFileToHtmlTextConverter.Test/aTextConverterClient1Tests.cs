1e79352a411785a0b51402b059b623aad8ddba76
azure cuba <azurecuba@gmail.com>
2023-07-15 18:48:47 +0530

    Refactored code for UnicodeFileToHtmlTextConverter and added test cases

---
+++ src/UnicodeFileToHtmlTextConverter.Test/aTextConverterClient1Tests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using TDDMicroExercises.UnicodeFileToHtmlTextConverter.SomeDependencies;
+
+namespace TDDMicroExercises.Tests
+{
+    [TestFixture]
+    public class aTextConverterClient1Tests
+    {
+        [Test]
+        public void Constructor_ConvertsTextToHtml()
+        {
+            // Arrange
+
+            // Act
+            var client = new aTextConverterClient1();
+
+            // Assert           
+            Assert.NotNull(client);
+        }
+    }
+}
--- src/UnicodeFileToHtmlTextConverter/UnicodeFileToHtmlTextConverter.cs
+++ src/UnicodeFileToHtmlTextConverter/UnicodeFileToHtmlTextConverter.cs
@@ -1,12 +1,24 @@
-using System.IO;
+﻿using System.IO;
+using System.Text;
 using System.Web;
 
 namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
 {
+    public interface IFileReader
+    {
+        string[] ReadAllLines(string path);
+    }
+
+    public interface IHtmlEncoder
+    {
+        string HtmlEncode(string value, bool isLastLine = false);
+    }
+
     public class UnicodeFileToHtmlTextConverter
     {
         private readonly string _fullFilenameWithPath;
-
+        private IFileReader _fileReader;
+        private IHtmlEncoder _htmlEncoder;
 
         public UnicodeFileToHtmlTextConverter(string fullFilenameWithPath)
         {
@@ -15,20 +27,45 @@ namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
 
         public string ConvertToHtml()
         {
-            using (TextReader unicodeFileStream = File.OpenText(_fullFilenameWithPath))
+            _fileReader = new FileReader();
+            _htmlEncoder = new HtmlEncoder();
+            StringBuilder htmlBuilder = new StringBuilder();
+            string html = string.Empty;
+            string[] lines = _fileReader.ReadAllLines(_fullFilenameWithPath);
+            for (var i = 0; i < lines.Length; i++)
             {
-                string html = string.Empty;
-
-                string line = unicodeFileStream.ReadLine();
-                while (line != null)
+                if (i == lines.Length - 1)
                 {
-                    html += HttpUtility.HtmlEncode(line);
-                    html += "<br />";
-                    line = unicodeFileStream.ReadLine();
+                    html += _htmlEncoder.HtmlEncode(lines[i], true);
+                }
+                else
+                {
+                    html += _htmlEncoder.HtmlEncode(lines[i]);
                 }
 
-                return html;
             }
+
+
+            return html;
+        }
+    }
+
+    // Implementation of IFileReader using System.IO
+    public class FileReader : IFileReader
+    {
+        public string[] ReadAllLines(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+    }
+
+    // Implementation of IHtmlEncoder using System.Web
+    public class HtmlEncoder : IHtmlEncoder
+    {
+        public string HtmlEncode(string value, bool isLastLine = false)
+        {
+
+            return HttpUtility.HtmlEncode(value) + (isLastLine ? "" : "<br />");
         }
     }
 }
